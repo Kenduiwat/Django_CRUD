ce6d9a9b962b8501ae10cc8d339d6adb3311d543
Kenduiwat <vkorir.143@gmail.com>
2022-11-09 23:03:55 +0300

    This contains the rest framework_API
    This contains the rest framework_API

---
+++ SONG_CRUD/musicapp/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Song, Artiste, Lyrics
+
+class ArtisteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artiste
+        fields = ('__all__')
+        
+        
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ('__all__')
+
+
+class LyricsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lyrics
+        fields = ('__all__')
--- Song_Crud/musicapp/models.py
+++ Song_Crud/musicapp/models.py
@@ -7,7 +7,9 @@ class Artiste (models.Model):
     FirstName = models.CharField(max_length=50)
     LastName = models.CharField(max_length=50)
     Age = models.IntegerField()
-
+'''
+    def __str__(self):
+        return self.FirstName + ' ' + self.LastName'''
     
 
 class Song (models.Model):
@@ -17,6 +19,8 @@ class Song (models.Model):
     Likes = models.IntegerField()
     #Artiste_id = models.CharField(max_length=50)
 
+    '''def __str__(self):
+        return self.Artiste_id'''
 
 
 
@@ -24,3 +28,6 @@ class Lyrics (models.Model):
     Song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     Content = models.TextField()
     #Song_id = models.CharField(max_length=50)
+
+    '''def __str__(self):
+        return self.Song_id'''
